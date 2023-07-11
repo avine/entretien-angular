a09a8fb9670cae6b6a82d953ac7e81d18b9d6272
Stéphane Francel <stephane.francel@gmail.com>
2023-07-11 12:35:38 +0200

    Scaffold App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface TodosResponseDto {
+  todos: {
+    id: number;
+    userId: number;
+    todo: string;
+    completed: any;
+  }[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'entretien';
+  todos: TodosResponseDto['todos'] = [];
+
+  constructor(private httpClient: HttpClient) {
+    httpClient.get<TodosResponseDto>('https://dummyjson.com/todos?limit=5').subscribe((response: TodosResponseDto) => {
+      this.todos = response.todos;
+    });
+  }
+
+  remainingTodosCount() {
+    let count = 0;
+    for (let i = 0; i < this.todos.length; i++) {
+      if (!this.todos[i].completed) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  checkboxId(todo: TodosResponseDto['todos'][0]) {
+    return 'check-' + todo.id;
+  }
+
+  toggleTodo(todo: TodosResponseDto['todos'][0]) {
+    todo.completed = !todo.completed;
+  }
 }
